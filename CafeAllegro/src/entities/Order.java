3397d25e92796cc89079733ae74eaadc941f293b
Brian Thomas <brianraythomas@gmail.com>
2017-09-30 13:21:05 -0600

    organized imports to remove warnings

--- CafeAllegro/src/entities/Order.java
+++ CafeAllegro/src/entities/Order.java
@@ -2,15 +2,11 @@ package entities;
 
 import java.sql.Date;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
