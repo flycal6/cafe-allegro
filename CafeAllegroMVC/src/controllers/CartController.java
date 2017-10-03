6ace86b494bb1f68efa375253fa6cc61b10e9391
Stanton Viaduc <stan@Stantons-MacBook-Pro.local>
2017-10-03 09:02:05 -0600

    Cart jsp works

--- CafeAllegroMVC/src/controllers/CartController.java
+++ CafeAllegroMVC/src/controllers/CartController.java
@@ -4,29 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import data.OrderDAO;
-import entities.MenuItem;
 
 @Controller
 public class CartController {
 
 	@Autowired
 	private OrderDAO orderDAO;
-
-//	@RequestMapping(path="cart.do", method=RequestMethod.POST)
-//	public ModelAndView cartSummary(MenuItem menuItem) {
-//		ModelAndView mv = new ModelAndView();
-//		mv.addObject("items", orderDAO.addingItemtoCart(menuItem));
-//		mv.setViewName("views/cart.jsp");
-//		return mv;
-//	}
 	
-	@RequestMapping(path="cart.do", method=RequestMethod.GET)
-	public ModelAndView addItemById(int id) {
+	@RequestMapping(path="cart.do", method=RequestMethod.POST)
+	public ModelAndView addItemById (@RequestParam(value="itemId") int itemId) {
 		ModelAndView mv = new ModelAndView();
-		mv.addObject("items", orderDAO.getMenuItemById(id));
+		mv.addObject("items", orderDAO.getMenuItemById(itemId));
+		
 		mv.setViewName("views/cart.jsp");
 		return mv;
 	}
