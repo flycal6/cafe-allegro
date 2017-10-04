533206b10aa634df2050ec4fb3b2db36e778748d
Chris Bartkewicz <christopher@irreverentwarriors.com>
2017-10-03 18:03:26 -0600

    order is now presisting

--- CafeAllegroMVC/src/controllers/CartController.java
+++ CafeAllegroMVC/src/controllers/CartController.java
@@ -2,17 +2,16 @@ package controllers;
 
 import javax.servlet.http.HttpSession;
 
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import cart.Cart;
 import data.CartDAO;
-import data.OrderDAO;
+import data.MenuItemDAO;
 import entities.MenuItem;
 import entities.User;
 
@@ -21,6 +20,11 @@ public class CartController {
 
 	@Autowired
 	private CartDAO cartDAO;
+
+	@Autowired
+	private MenuItemDAO miDao;
+	
+	
 
 	@RequestMapping(path = "showCart", method = RequestMethod.GET)
 	public String showCart(HttpSession session) {
@@ -31,14 +35,17 @@ public class CartController {
 	}
 
 	@RequestMapping(path = "addToCart.do", method = RequestMethod.POST) // Coming from the INDEX.JSP
-	public String addItemById(RedirectAttributes redir, MenuItem item, HttpSession session) { // index.jsp
+	public ModelAndView addItemById(RedirectAttributes redir, MenuItem item, HttpSession session) { // index.jsp
 		Cart sessionCart = (Cart) session.getAttribute("cart");
 		if (sessionCart == null) {
 			sessionCart = cartDAO.createNewCart();
 		}
 		redir.addFlashAttribute("cart", sessionCart);
 		session.setAttribute("cart", cartDAO.addMenuItemToCart(sessionCart, item));
-		return "redirect:AddedToCart.do";
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("items", miDao.getAllItemsforIndexPage());
+		mv.setViewName("views/index.jsp");
+		return mv;
 	}
 	
 	@RequestMapping(path = "AddedToCart.do", method = RequestMethod.GET) // Coming from the INDEX.JSP
