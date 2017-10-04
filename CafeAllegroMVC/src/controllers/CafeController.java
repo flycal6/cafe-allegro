0fab0d91642812afce13180938c3a5e0918d8abc
Brian Thomas <brianraythomas@gmail.com>
2017-10-04 09:34:26 -0600

    controllers reverted to working

--- CafeAllegroMVC/src/controllers/CafeController.java
+++ CafeAllegroMVC/src/controllers/CafeController.java
@@ -1,31 +1,23 @@
 package controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import data.MenuItemDAO;
-import data.UserDAO;
-import entities.User;
 
 @Controller
 public class CafeController {
 
 	@Autowired
 	private MenuItemDAO menuItemDao;
-//	@Autowired
-//	private UserDAO userDao;
-	
-	@RequestMapping(path="Cafe.do", method=RequestMethod.GET)
+
+	@RequestMapping(path = "Cafe.do", method = RequestMethod.GET)
 	public String showHomePage(Model model) {
 		model.addAttribute("items", menuItemDao.getAllItemsforIndexPage());
 		return "views/index.jsp";
 	}
 
-	
-	
-	
 }
